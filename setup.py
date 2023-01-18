e007fbdbe9676edc97a379945bdd042d0eee3a56
Bába Kristóf <kristof@Babas-MacBook-Pro.local>
2023-01-18 18:54:07 +0200

    setup.py updated

---
+++ setup.py
@@ -0,0 +1,6 @@
+# add the end part of the YT playlist link, like: PLCzGEgWUjzlh5gtA-WDWGNcZQIz6VtEUU from https://www.youtube.com/playlist?list=PLCzGEgWUjzlh5gtA-WDWGNcZQIz6VtEUU
+yt_playlist = ""
+# add your own YT api key
+yt_api_key = ""
+# add your own Spotify api key
+spoti_api_key = ""
