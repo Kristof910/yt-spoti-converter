8ae697acc12a8a8da5e3443620d0a01db700d1ba
Bába Kristóf <kristof@Babas-MacBook-Pro.local>
2023-01-20 14:07:52 +0200

    spoti api done

--- api.py
+++ api.py
@@ -5,11 +5,11 @@ def get_yt_songs(api_key, playlist_link):
     try:
         url = f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={playlist_link}&key={api_key}"
 
+        list = []
         while True:
             response = requests.get(url)
             data = json.loads(response.text)
 
-            list = []
             for item in data["items"]:
                 list.append(item["snippet"]["title"])
 
@@ -23,8 +23,40 @@ def get_yt_songs(api_key, playlist_link):
     except Exception as e:
         print(f"Ops, something gone wrong: {e}")
 
-def create_spoti_playlist(api_key):
-    pass
+def create_spoti_playlist(token, user_id, playlist_name):
+    try:
+        headers = {
+            "Authorization": "Bearer " + token,
+            "Content-Type": "application/json"
+        }
 
-def add_spoti_songs(api_key, list):
-    pass
+        playlist_data = {
+            "name": playlist_name,
+            "description": "test"
+        }
+
+        response = requests.post(f"https://api.spotify.com/v1/users/{user_id}/playlists", headers=headers, data=json.dumps(playlist_data))
+        return response.json()['id']
+
+    except Exception as e:
+        print(f"Ops, something gone wrong: {e}")
+
+def add_spoti_songs(token, list, playlist_id):
+    access_token = token
+
+    # List of song names
+    song_list = list
+
+    # Search for songs and add them to the playlist
+    for song in song_list:
+        # Search for song
+        headers = {'Authorization': 'Bearer ' + access_token}
+        search_url = 'https://api.spotify.com/v1/search?q={}&type=track&limit=1'.format(song)
+        response = requests.get(search_url, headers=headers)
+        song_id = response.json()['tracks']['items'][0]['id']
+
+        # Add the song to the playlist
+        headers = {'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json'}
+        add_url = 'https://api.spotify.com/v1/playlists/{}/tracks?uris=spotify:track:{}'.format(playlist_id,song_id)
+        response = requests.post(add_url, headers=headers)
+        print(response)
--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from api import *
 import setup
 
 def main():
-    yt_songs = get_yt_songs(setup.yt_api_key, setup.yt_playlist))
+    yt_songs = get_yt_songs(setup.yt_api_key, setup.yt_playlist)
+    playlist_id = create_spoti_playlist(setup.spoti_token, setup.spoti_user_id, "YT Converted")
+    add_spoti_songs(setup.spoti_token, yt_songs, playlist_id)
+    
 
 if __name__ == '__main__':
     main()
